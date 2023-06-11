fe875c79fb1f09bceadd4d1f99cc36f70051d0fa
Terceros <diego.terceros@ikhon.com.br>
2023-06-10 21:40:03 -0400

    criacao do layout inicial do modulo de review

---
+++ app/src/main/java/com/example/myapplication/model/model/Review.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.model.model
+
+import java.time.LocalDateTime
+
+class Review (id : Int? = null, var data : LocalDateTime, var idRestaurante : Int? = null, var localizacao : String, var nota: Double){
+}
